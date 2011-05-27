fbcde948311db8a2f890fc3511f8d80f66736d2d
mindvalley <mindvalley@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-27 10:43:29 +0000

    initial & tagging of 1.0
    git-svn-id: https://plugins.svn.wordpress.org/mindvalley-post-get-variable/tags/1.0@389695 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ post-get-variables.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Plugin Name: Mindvalley Post & Get Variables
+Description: Lets you output a POST or GET variable in the page via shortcode.
+Author: MindValley
+Version: 1.0
+*/
+
+/*
+ * Example :
+ * 
+ * <script type="text/javascript">
+ *      var pageTracker = _gat._getTracker("UA-XXXXX-1");
+ *      pageTracker._trackPageview();
+ *      pageTracker._addTrans(
+ *          "[post_var name='txn_id']", // Order ID
+ *          "[post_var name='aff']", // Affiliation
+ *          "[post_var name='total']", // Total
+ *          "[post_var name='tax']", // Tax
+ *          "[post_var name='shipping']", // Shipping
+ *          "[post_var name='city']", // City
+ *          "[post_var name='state']", // State
+ *          "[post_var name='country']" // Country
+ *      );
+ * </script>
+ * 
+ */
+Class MV_Post_Get_Variables {
+    function __construct(){
+        add_shortcode('post_var', array(&$this,'post_var'));
+        add_shortcode('get_var', array(&$this,'get_var'));
+    }
+    
+    function post_var($atts){
+        if(isset($_POST[$atts['name']])){
+            return htmlentities($_POST[$atts['name']]);
+        }
+        return '';
+    }
+    
+    function get_var($atts){
+        if(isset($_GET[$atts['name']])){
+            return htmlentities($_GET[$atts['name']]);
+        }
+        return '';
+    }
+}
+
+new MV_Post_Get_Variables();
