643a8e2edb281f33d4c58cc3564f01acb4eccd34
mindvalley <mindvalley@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-24 10:00:14 +0000

    support for array
    git-svn-id: https://plugins.svn.wordpress.org/mindvalley-post-get-variable/trunk@400787 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- post-get-variables.php
+++ post-get-variables.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Mindvalley Post & Get Variables
 Description: Lets you output a POST or GET variable in the page via shortcode.
-Author: MindValley
-Version: 1.0.1
+Author: Mindvalley
+Version: 1.0.2
 */
 
 /*
@@ -24,6 +24,12 @@ Version: 1.0.1
  *      );
  * </script>
  * 
+ * For array values :
+ * 
+ * [post_var] name=search[user][email] [/post_var]
+ * [get_var] name=search[user][first_name] [/get_var]
+ * [get_var] name=search[user][last_name] [/get_var]
+ * 
  */
 Class MV_Post_Get_Variables {
     function __construct(){
@@ -32,17 +38,78 @@ Class MV_Post_Get_Variables {
     }
     
     function post_var($atts){
-        if(isset($_POST[$atts['name']])){
-            return htmlentities($_POST[$atts['name']]);
+        if(!is_array($atts)){
+			$atts = array();
+		}
+		$atts = $this->convert_block_to_params($atts,$content);
+		$key = $atts['name'];
+		
+		// Array Key
+		if($pos = strpos($key,'[')){
+			$basekey = substr($key,0,$pos);
+			$remainder = substr($key,$pos);
+			
+			eval('$value = $_POST[' . $basekey . ']' . $remainder . ';');
+		}else{
+			$value = $_POST[$key];
+		}
+		
+        if(!empty($value)){
+            return htmlentities($value);
         }
         return '';
     }
     
-    function get_var($atts){
-        if(isset($_GET[$atts['name']])){
-            return htmlentities($_GET[$atts['name']]);
+    function get_var($atts,$content){
+		if(!is_array($atts)){
+			$atts = array();
+		}
+		$atts = $this->convert_block_to_params($atts,$content);
+		$key = $atts['name'];
+		
+		// Array Key
+		if($pos = strpos($key,'[')){
+			$basekey = substr($key,0,$pos);
+			$remainder = substr($key,$pos);
+			
+			eval('$value = $_GET[' . $basekey . ']' . $remainder . ';');
+		}else{
+			$value = $_GET[$key];
+		}
+		
+        if(!empty($value)){
+            return htmlentities($value);
         }
         return '';
+    }
+
+	function convert_block_to_params($params,$text){
+		// Wordpress Funky Encoding
+		$text = str_replace("&#8221;",'"',$text);
+		$text = str_replace("&#8243;",'"',$text);
+		$text = str_replace("–","-",$text);
+		$text = str_replace('″','"',$text);
+		$text = str_replace("’","'",$text);
+		
+		$values = array();
+        $search = array(chr(145), chr(146), chr(147), chr(148), chr(151));
+        $replace = array("'", "'", '"', '"', '-');
+        $text = str_replace($search, $replace, $text);
+        $text = str_replace('”','"',$text);
+        $text = str_replace('\\"',"&quot;",$text);
+        $text = str_replace("\\'","&#39;",$text);
+        $text = str_replace('\\$','$',$text);
+        $text = str_replace('\\{','{',$text);
+        $text = str_replace('\\}','}',$text);
+        $text = str_replace("<br />\n","\n ",$text);
+        $text = str_replace("<p>","",$text);
+        $text = str_replace("</p>","",$text);
+		
+		if (preg_match_all('/([^=\s]+)(\s|)=(\s|)("(?P<value1>[^"]+)"|\'(?P<value2>[^\']+)\'|(?P<value3>.+?)\b)/', $text, $matches, PREG_SET_ORDER))
+            foreach ($matches as $match)
+                $values[trim($match[1])] = trim(@$match['value1'] . @$match['value2'] . @$match['value3']);
+
+		return array_merge_recursive($params,$values);
     }
 }
 
